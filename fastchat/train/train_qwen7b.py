9004afa22194305162ed402e743bbc78d4184044
heyong <heyong@yidian-inc.com>
2023-08-24 16:30:27 +0800

    fix

--- fastchat/train/train_qwen7b.py
+++ fastchat/train/train_qwen7b.py
@@ -122,14 +122,13 @@ def preprocess(
     for source, conversation, target in zip(sources, conversations, targets):
         starts = (target == tokenizer.im_start_id).nonzero(as_tuple=False)
         ends = (target == tokenizer.im_end_id).nonzero(as_tuple=False)
-        if len(starts) != len(ends):
+        if len(starts) == len(ends) and len(source) != len(starts) - 1:
             target[:] = IGNORE_TOKEN_ID
             rank0_print(
                 f"WARNING: truncation or special tokenization mismatch: len(source): {len(source)}, len(starts): {len(starts)}, len(ends): {len(ends)}"
                 f" (ignored)"
             )
             continue
-
         for i, (start, end) in enumerate(zip(starts, ends)):
             if target[start+1] == assistant_token:
                 target[start: start + 3] = IGNORE_TOKEN_ID
